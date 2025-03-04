3da20521fdf10bab17fb711e3a6699a5bdc320ba
s31426 <s31426@pjwstk.edu.pl>
2025-03-04 16:51:42 +0100

    Mod 4

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-mod1;111
-    mod2;
-mod3;
+
+
+
+static double GetAvrage(int[] arr)
+{
+    return 0;
+}
+
+int[] arr= {1,2,3,4,5,6,7};
+Console.WriteLine(GetAvrage(arr));
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+}
